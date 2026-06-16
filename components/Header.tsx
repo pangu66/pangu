9c4183a397d85c3b15cd421c9ed748e8d95134f8
pangu66 <ademmihoubi99@gmail.com>
2026-06-16 10:43:22 +0100

    components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        padding: "20px",
+        borderBottom: "1px solid #222",
+        fontSize: "28px",
+        fontWeight: "bold"
+      }}
+    >
+      PANGU
+    </header>
+  );
+}
